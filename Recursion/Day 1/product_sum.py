46c74541ec740ac946ae48dd40fa65bac156412b
P-Bharti20 <70098320+P-Bharti20@users.noreply.github.com>
2021-06-27 14:19:25 +0530

    program to print product sum of special array

--- Recursion/Day 1/product_sum.py
+++ Recursion/Day 1/product_sum.py
@@ -2,3 +2,15 @@
 #A "special" array is a non-empty array that contains either integers or other "special" arrays. The product sum of a "special" array is the sum of its elements, 
 #where "special" arrays inside it are summed themselves and then multiplied by their level of depth.
 
+array=[5,2,[7,-1],3,[6,[-13,8],4]]
+
+def productSum(arr, level=1):
+    sum=0
+    for elem in arr:
+        if type(elem) is list:
+            sum+=productSum(elem, level+1)
+        else:
+            sum+=elem
+    return sum*level
+
+print(productSum(array))
