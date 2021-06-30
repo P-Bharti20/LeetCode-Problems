32c32c7dcac5dbdf70634bb83c4c74afe4bde5c7
P-Bharti20 <70098320+P-Bharti20@users.noreply.github.com>
2021-06-30 15:31:39 +0530

    program to print all subsets of a given set

--- Recursion/Day 2/powerset.py
+++ Recursion/Day 2/powerset.py
@@ -4,3 +4,15 @@ The powerset P(X) of a set X is the set of all subsets of X.
 For example, the powerset of [1,2] is [[], [1], [2], [1,2]].
 '''
 
+def powerSet(arr):
+    if len(arr) == 0:
+        return [[]]
+    else:
+        base = powerSet(arr[:-1])
+        operator = arr[-1:]
+        return base+[ (b+operator) for b in base]
+
+array = list(map(int,input().split()))
+print("Power Set: ", powerSet(array))
+
+
