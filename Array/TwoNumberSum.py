def22c1d0d95905c8f057c0fb4f5848004037f16
P-Bharti20 <70098320+P-Bharti20@users.noreply.github.com>
2021-09-16 01:43:36 +0530

    Update TwoNumberSum.py

--- Array/TwoNumberSum.py
+++ Array/TwoNumberSum.py
@@ -1,6 +1,22 @@
 #naive approach
+#O(n^2) Time / O(1) Space
 def twoNumberSum(arr,targetSum):
   for i in range(len(arr)-1):
     for j in range(i,len(arr)):
       return [arr[i],arr[j]]
+  return []
+
+
+#Efficient Approach
+#O(nlogn) Time / O(1) Space
+def twoNumberSum(arr,targetSum):
+  arr.sort()
+  l,r=0,len(arr)-1
+  while(l<r):
+    if arr[l]+arr[r]==targetSum:
+      return [arr[i],arr[j]]
+    elif arr[l]+arr[r]<targetSum:
+      l+=1
+    else:
+      r-=1
   return []
