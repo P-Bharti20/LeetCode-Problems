a3efcf03b239d0c41ec2670c0113aca63b9df436
P-Bharti20 <priyankabharti2001@gmail.com>
2024-02-18 03:20:24 +0530

    unique path optimized to O(m)

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -4,36 +4,17 @@
  * [62] Unique Paths
  */
 
-//Using memoization
+//O(m+n) solution
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
-    int f(int r,int c,vector<vector<int>> &dp){
-        if(r==0&&c==0) return 1;
-        if(r<0||c<0) return 0;
-    
-        if(dp[r][c]!=-1)
-            return dp[r][c];
-        int up=f(r-1,c,dp);
-        int left=f(r,c-1,dp);
-        return dp[r][c]=up+left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0&&j==0){
-                    dp[i][j]=1;
-                }else{
-                    int up=0,left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
-                }
-            }
+        double res=1;
+        for(int i=1;i<m;i++){
+           res=res*(n-1+i)/i;
         }
-        return dp[m-1][n-1];
+        return (int)res;
     }
 };
 
