e8659a6fdab2d54994863d998bd1f4a4bb209de5
P-Bharti20 <priyankabharti2001@gmail.com>
2024-03-12 02:55:54 +0530

    DP on strings

--- Dynamic Programming/72.edit-distance.cpp
+++ Dynamic Programming/72.edit-distance.cpp
@@ -1,16 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-string str="";
-int i=0,j=0,index=0;
-while(i<n && j<m){
-    if(ans[index]!=s1[i]){
-        str=str+s1[i];
-        i++;
-    }else{
-        str=str+ans[index];
-        index++;
-        i++;
-        j++;
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<int> prev(m+1,0),curr(m+1,0);
+        for(int i=0;i<=m;i++)  prev[i]=i;
+        for(int i=1;i<=n;i++){
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                     curr[j]=prev[j-1];
+                }else{
+                    curr[j]=1+ min(prev[j-1],min(prev[j],curr[j-1]));
+                }
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
-
-}
+};
