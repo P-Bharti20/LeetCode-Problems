31946f96c980611928f699864da2273685f14c55
P-Bharti20 <priyankabharti2001@gmail.com>
2024-03-14 01:53:51 +0530

    LCS using Binary Search

--- Dynamic Programming/300.longest-increasing-subsequence.cpp
+++ Dynamic Programming/300.longest-increasing-subsequence.cpp
@@ -2,30 +2,36 @@
 using namespace std;
 class Solution {
 public:
-    //Memoization
-    int f(int idx, int prev, vector<int>& nums,vector<vector<int>> &dp ){
-        if(idx >= nums.size()) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        dp[idx][prev+1]=f(idx+1,prev,nums,dp);
-        if(prev==-1 || nums[idx]>nums[prev]){
-            dp[idx][prev+1]= max(dp[idx][prev+1],1+f(idx+1,idx,nums,dp));
-        }
-        return dp[idx][prev+1];
-    }
-
-    //Tabulation
+    //using tabulation & space optimization
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> curr(n+1,0), next(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                curr[prev+1]=next[prev+1];
-                if(prev==-1 || nums[i]>nums[prev]){
-                    curr[prev+1]=max(curr[prev+1],1+next[i+1]);
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],1+dp[j]);
                 }
             }
-            next=curr;
+            maxi=max(maxi,dp[i]);
         }
-        return next[0];
+        return maxi;
+    }
+
+    //Using binary search
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i]>dp.back()){
+                dp.push_back(nums[i]);
+            }
+            else{
+                int idx = lower_bound(dp.begin(),dp.end(),nums[i]) - dp.begin();
+                dp[idx]=nums[i];
+            }
+        }
+        return dp.size();
     }
 };
