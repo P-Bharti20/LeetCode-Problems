afa642b9a8d3cf52bedc5fee66c0b9764e3243e5
P-Bharti20 <priyankabharti2001@gmail.com>
2024-12-06 19:24:59 +0530

    refactoring

---
+++ FAQs/42.trapping-rain-water.cpp
@@ -0,0 +1,14 @@
+/*
+The key to solving the “Trapping Rain Water” problem is recognizing that the amount of water 
+trapped at any position depends on the minimum height of the tallest bars on its left and right.
+*/
+
+// @lc code=start
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+    }
+};
+// @lc code=end
+
